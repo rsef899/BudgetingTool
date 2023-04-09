3d2054a26576ca284f8cd573a3705862bd2b07ce
rsef899 <rsef899@aucklanduni.ac.nz>
2023-04-09 13:20:11 +1200

    made the wbpages header

---
+++ front_btool/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './images/logo.jpg';
+
+function Header() {
+  return (
+    <header>
+        <div className = "logo-container">
+            <img src={logo} alt="Logo" className='logo'/>
+            <h1 className='header-text'>Budget-er</h1>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
