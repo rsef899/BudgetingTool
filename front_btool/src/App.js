26e29b5d158a751c86c8775e5e61c87762536f37
rsef899 <rsef899@aucklanduni.ac.nz>
2023-04-15 22:12:54 +1200

    finished starting screen, made it the first page that appears

--- front_btool/src/App.js
+++ front_btool/src/App.js
@@ -11,6 +11,8 @@ import LoadingScreen from './LoadingScreen';
 
 
 function App() {
+/*******After Main Loading Screen */
+const[onHomeScreen, setOnHomeScreen] = useState(true)
 /*******entry Change detection */
   const [entriesChange, setEntriesChange] = useState(0);
   //function to add entry
@@ -43,43 +45,52 @@ function App() {
 
   return (
     <div>
-      <LoadingScreen/>
-      {/* <ResetBack addhasReset={addhasReset}/>
-      <Header />  
-      <div class="main-grid-container">   
-          <div className='grid-container-mover'>
-            <div class="grid-container">     
-                <div className='entry-div'>
-                  <Entry addEntryChange ={addEntryChange} />
-                </div>
-                
-                  <div className='net-balance-div'>
-                    <NetBalance pressedEnter={pressedEnter} entriesChange={entriesChange} hasReset={hasReset} 
-                    updateSoldFlag={updateSoldFlag} entryUpdated={entryUpdated}/>
-                  </div>
+      {onHomeScreen ? (
+        <>
+         <LoadingScreen setOnHomeScreen={setOnHomeScreen}/>
+         <ResetBack addhasReset={addhasReset}/>
+        </>
+        ):(
+          <>
+            <Header />  
+            <div class="main-grid-container">   
+                <div className='grid-container-mover'>
+                  <div class="grid-container">     
+                      <div className='entry-div'>
+                        <Entry addEntryChange ={addEntryChange} />
+                      </div>
+                      
+                        <div className='net-balance-div'>
+                          <NetBalance pressedEnter={pressedEnter} entriesChange={entriesChange} hasReset={hasReset} 
+                          updateSoldFlag={updateSoldFlag} entryUpdated={entryUpdated}/>
+                        </div>
+                        
+                          {entriesChange ? (
+                            <div className='update-div'>
+                              <UpdateInfo entriesChange={entriesChange} pressedEnter={pressedEnter} updateEntryLog={updateEntryLog}
+                              setupdateSoldFlag={setupdateSoldFlag} setupdatedSoldIndex={setupdatedSoldIndex} 
+                              setChangedDetail={setChangedDetail} changedDetail={changedDetail} 
+                              entryUpdated={entryUpdated} />
+                              </div> ):(null)}  
+                      </div>
                   
-                    {entriesChange ? (
-                      <div className='update-div'>
-                        <UpdateInfo entriesChange={entriesChange} pressedEnter={pressedEnter} updateEntryLog={updateEntryLog}
-                        setupdateSoldFlag={setupdateSoldFlag} setupdatedSoldIndex={setupdatedSoldIndex} 
-                        setChangedDetail={setChangedDetail} changedDetail={changedDetail} 
-                        entryUpdated={entryUpdated} />
-                        </div> ):(null)}  
                 </div>
-            
-          </div>
-          <div className="entire-top-table-div">
-                {!entriesChange ? (
-                  <h1 className='EmptyEntryHeader'>Enter an entry to get started</h1>):(
-                  //only one componant can be returned in a ternery operator hence <>, allows both to render 
-                  <>
-                    <Table entriesChange={entriesChange} entryUpdated={entryUpdated}/>
-                    <SoldItems entriesChange={entriesChange} prop2={addEnterPressedChange} 
-                    updateSoldFlag={updateSoldFlag} setupdateSoldFlag={setupdateSoldFlag} updatedSoldIndex={updatedSoldIndex}
-                    changedDetail={changedDetail} />
-                  </>)}
-              </div>
-      </div> */}
+                <div className="entire-top-table-div">
+                      {!entriesChange ? (
+                        <h1 className='EmptyEntryHeader'>Enter an entry to get started</h1>):(
+                        //only one componant can be returned in a ternery operator hence <>, allows both to render 
+                        <>
+                          <Table entriesChange={entriesChange} entryUpdated={entryUpdated}/>
+                          <SoldItems entriesChange={entriesChange} prop2={addEnterPressedChange} 
+                          updateSoldFlag={updateSoldFlag} setupdateSoldFlag={setupdateSoldFlag} updatedSoldIndex={updatedSoldIndex}
+                          changedDetail={changedDetail} />
+                        </>)}
+                    </div>
+            </div>
+            </>
+        )}
+     
+      
     {/*MainDiv end */}
     </div>  
   );
--- front_btool/src/LoadingScreen.js
+++ front_btool/src/LoadingScreen.js
@@ -2,7 +2,11 @@ import './LoadingScreen.css';
 import React from "react";
 import logo from './images/logo.jpg';
 
-function LoadingScreen(){
+function LoadingScreen(props){
+
+    const handleLoginButton = (event) => {
+        props.setOnHomeScreen(false);
+    }
 
     return(
          <div className='loading-Screen-Container'>
@@ -10,13 +14,13 @@ function LoadingScreen(){
             <div className="use-Lite-grid-cont">
             <h1 className='Loading-Screen-Header'>Budget-er</h1>
                 <h1 className='Lite-Selection-Header'>Click to use</h1>
-                <button className= "LiteButton" type="submit" >Budget-er Lite</button>
+                <button className= "LiteButton" type="submit" onClick={handleLoginButton} >Budget-er Lite</button>
             </div>
             <div class="form-container">
                 <form>
-                    <label for="username">Username:</label>
+                    <label htmlFor="username">Username:</label>
                     <input type="text" id="username" name="username"></input>
-                    <label for="password">Password:</label>
+                    <label htmlFor="password">Password:</label>
                     <input type="password" id="password" name="password"></input>
                 </form>
             </div>
