0a2772e82a59e77bdf208a52c0d2fbdf880b59f7
bwei551crazy <crazykwaibao@gmail.com>
2024-02-24 04:01:10 +1300

    Added small design changes to PcPage layout

--- front_btool/src/PCEntry.js
+++ front_btool/src/PCEntry.js
@@ -7,15 +7,19 @@ const PCEntry = ({ pc,onClick }) => {
     }
     return (
         <div className="pcBox" onClick={handleClick}>
-            <h3>{pc.name}</h3>
-            <p>Total Price: ${pc.total_price}</p>
-            <ul>
-                {pc.components.map(component => (
-                    <li key={component.id} className="componentListLayout">
-                        {component.component_type}: {component.name} - Price: ${component.price}
-                    </li>
-                ))}
-            </ul>
+            <div className = "pcName">
+                <h3>  {pc.name}</h3>
+                <div className = "total_price" >
+                <p>Total Price: ${pc.total_price}</p>
+                <ul>
+                    {pc.components.map(component => (
+                        <li key={component.id} className="componentListLayout">
+                            {component.component_type}: {component.name} - Price: ${component.price}
+                        </li>
+                    ))}
+                </ul>
+                </div>
+            </div>
         </div>
     );
 };
